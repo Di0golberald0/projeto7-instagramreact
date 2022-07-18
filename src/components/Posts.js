fe5f11a1a690c00d096bdac7a5aa33fe338da976
Di0golberald0 <diogolberaldo@gmail.com>
2022-07-18 12:49:29 -0300

    Todos Maps Feitos

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,31 @@
 export default function Posts() {
 
+  const posts = [
+    {
+      "usuarioImg" : "assets/img/meowed.svg",
+      "usuarioName" : "meowed",
+      "conteudoImg" : "assets/img/gato-telefone.svg",
+      "curtidasImg" : "assets/img/respondeai.svg",
+      "curtidasName" : "respondeai",
+      "curtidasNumero" : "outras 101.523 pessoas",
+    },
+    {
+      "usuarioImg" : "assets/img/barked.svg",
+      "usuarioName" : "barked",
+      "conteudoImg" : "assets/img/dog.svg",
+      "curtidasImg" : "assets/img/adorable_animals.svg",
+      "curtidasName" : "adorable_animals",
+      "curtidasImg" : "outras 99.159 pessoas",
+    }
+  ]
   return (
     <div class="posts">
-      <div class="post">
+      {posts.map(post =>         
+    `<div class="post">
         <div class="topo">
           <div class="usuario">
-            <img src="assets/img/meowed.svg" />
-            meowed
+            <img src={post.usuarioImg} />
+            {post.usuarioName}
           </div>
           <div class="acoes">
             <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -14,7 +33,7 @@ export default function Posts() {
         </div>
         
         <div class="conteudo">
-          <img src="assets/img/gato-telefone.svg" />
+          <img src={post.conteudoImg} />
         </div>
         
         <div class="fundo">
@@ -31,49 +50,13 @@ export default function Posts() {
           </div>
           
           <div class="curtidas">
-            <img src="assets/img/respondeai.svg" />
+            <img src={post.curtidasImg} />
             <div class="texto">
-              Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+              Curtido por <strong>{post.curtidasName}</strong> e <strong>{post.curtidasNumero}</strong>
             </div>
           </div>
         </div>
-      </div>
-
-<div class="post">
-  <div class="topo">
-    <div class="usuario">
-      <img src="assets/img/barked.svg" />
-      barked
-    </div>
-    <div class="acoes">
-      <ion-icon name="ellipsis-horizontal"></ion-icon>
-    </div>
-  </div>
-
-  <div class="conteudo">
-    <img src="assets/img/dog.svg" />
-  </div>
-
-  <div class="fundo">
-    <div class="acoes">
-      <div>
-        <ion-icon name="heart-outline"></ion-icon>
-        <ion-icon name="chatbubble-outline"></ion-icon>
-        <ion-icon name="paper-plane-outline"></ion-icon>
-      </div>
-      <div>
-        <ion-icon name="bookmark-outline"></ion-icon>
-      </div>
-    </div>
-
-    <div class="curtidas">
-      <img src="assets/img/adorable_animals.svg" />
-      <div class="texto">
-        Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-      </div>
-    </div>
-  </div>
-</div>
+      </div>`)}
     </div>
   );
 }
